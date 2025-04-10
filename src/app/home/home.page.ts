b8aad2153120cd0da66431a9097cfed73854e2ac
Danny <g00437615@atu.ie>
2025-04-10 22:41:58 +0100

    Create getItems method and made a copy of items array from api in the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { ApiService } from '../services/api/api.service';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,16 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+
+  items: any[] = [];
+  allItems: any[] = [];
+
+  private api = inject(ApiService);
+
   constructor() {}
+
+  getItems() {
+   this.allItems = this.api.items;
+   this.items = [...this.allItems];
+  }
 }
