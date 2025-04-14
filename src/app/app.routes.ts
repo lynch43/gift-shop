f9c1572968fb6ca7c1cafe53e82c6a81acc4b6c4
Danny <g00437615@atu.ie>
2025-04-15 00:35:31 +0100

    Added back button to the ite-details html, cleared out the initial boilerplate. commiting as continuously getting 'unable to connect to ng in the cli

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,18 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    /*
+    * When user goes to /home, angular will scan this children array and will try and figure out what to load next
+    *
+    */
     path: 'home',
     children: [
       {
-        path: '',
+        path: '', // empty because it is the root page
         loadComponent: () => import('./home/home.page').then((m) => m.HomePage)
       },
       {
-        path: 'gifts/:id',
+        path: 'gifts/:id', // use this to path the ID
         loadComponent: () => import('./home/item-detail/item-detail.page').then(m => m.ItemDetailPage)
       }
 
