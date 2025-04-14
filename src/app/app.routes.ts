c62de01eee1a4ee6e52182ccb9685f0f0a7cbf1c
Danny <g00437615@atu.ie>
2025-04-15 00:18:14 +0100

    Edit app.routes.ts to make the item details page a child of the home page, also passing the item ID into the path of the individual item / gift

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./home/home.page').then((m) => m.HomePage)
+      },
+      {
+        path: 'gifts/:id',
+        loadComponent: () => import('./home/item-detail/item-detail.page').then(m => m.ItemDetailPage)
+      }
+
+    ]
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
-  },  {
-    path: 'item-detail',
-    loadComponent: () => import('./home/item-detail/item-detail.page').then( m => m.ItemDetailPage)
   },
+
 
 ];
