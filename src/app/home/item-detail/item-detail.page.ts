2e175d4c310f5673d0390037d8b1ba32109686d9
Danny <g00437615@atu.ie>
2025-04-15 01:35:14 +0100

    Add functionality to fetch 'id' parameter from route in ItemDetailPage componant, check if the id is missing or 0. then navigate if so. get item details and store it in item

--- src/app/home/item-detail/item-detail.page.ts
+++ src/app/home/item-detail/item-detail.page.ts
@@ -1,20 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonNav, IonButtons } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons } from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
+import { inject } from '@angular/core';
+import { NavController } from '@ionic/angular/standalone';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.page.html',
   styleUrls: ['./item-detail.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButtons, IonTitle, IonBackButton, IonButtons, IonToolbar, IonHeader, IonContent],
 })
 export class ItemDetailPage implements OnInit {
 
-  constructor() { }
+  private route = inject(ActivatedRoute);
+  private navCtrl = inject(NavController);
+  id!: string;
+  item: any;
+  private api = inject(ApiService);
 
   ngOnInit() {
+    this.getItem(); // fetch the item details
+
+  }
+
+  // get item from the route's parameter
+  // snapshot will get current route
+  // paramMap can grab the dynamic part which is 'id
+  getItem() {
+    const id = this.route.snapshot.paramMap.get('id');
+    console.log('check the id: ', id);
+    // check if id is not there or = 0 
+    if (!id || id == '0') {
+      this.navCtrl.back();
+      return;
+    }
+    this.id = id;
+
+    this.item = this.api.items.find((record) => record.id == id);
+    console.log(this.item);
   }
 
 }
